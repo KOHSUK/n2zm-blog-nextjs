e38c1e224a97ddb14e7013f0240ee80f1ce345e6
Kosuke Kihara <ashenden1965@gmail.com>
2023-11-06 17:37:01 +0900

    chore: added log

--- src/app/article/cover/route.ts
+++ src/app/article/cover/route.ts
@@ -3,8 +3,11 @@ import NotionToZennMd from 'notion-to-zenn-md';
 import CoverImageResponse from '../../../lib/cover-image-response';
 
 export async function GET(request: Request) {
+  console.log('request.url', request.url);
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
+
+  console.log('id', id);
 
   if (id == null) {
     return Response.error();
@@ -17,7 +20,11 @@ export async function GET(request: Request) {
 
   const n2zm = new NotionToZennMd(secret);
 
+  console.log('n2zm initialized');
+
   const { title } = await n2zm.getFrontMatter(id, NotionPropertyMappings);
+
+  console.log('title', title);
 
   return CoverImageResponse({ id, title });
 }
