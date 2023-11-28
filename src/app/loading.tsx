8273d64ce2f48f7c4500b9459c7d1acc1a553bb4
Kosuke Kihara <ashenden1965@gmail.com>
2023-11-28 21:11:55 +0900

    feat: created loading for home

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,15 +1,15 @@
 export default async function Loading() {
   return (
     <div className="xl:max-w-screen-xl md:max-w-screen-md mx-auto">
-      <div className="animate-pulse grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-8 md:gap-x-12 gap-y-20 md:gap-y-32">
+      <div className="animate-pulse grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-8 md:gap-x-12 gap-y-20 md:gap-y-24">
         {[...Array(6)].map((i) => (
           <div key={i}>
-            <div className="bg-gray-200 dark:bg-gray-700 aspect-w-16 aspect-h-9 rounded overflow-hidden shadow-lg relative mb-2"></div>
+            <div className="bg-gray-200 dark:bg-gray-700 aspect-w-16 aspect-h-9 rounded overflow-hidden relative mb-2"></div>
             <div className="bg-gray-200 dark:bg-gray-700 rounded-full w-20 h-4" />
-            <div className="bg-gray-200 dark:bg-gray-700 rounded-full w-full h-7 my-4" />
+            <div className="bg-gray-200 dark:bg-gray-700 rounded-full w-full h-6 my-4" />
             <div className="flex items-center">
               <div className="bg-gray-200 dark:bg-gray-700 rounded-full h-8 w-8 mr-2" />
-              <div className="bg-gray-200 dark:bg-gray-700 rounded-full h-6 w-20" />
+              <div className="bg-gray-200 dark:bg-gray-700 rounded-full h-5 w-20" />
             </div>
           </div>
         ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
   return (
     <div className="xl:max-w-screen-xl md:max-w-screen-md mx-auto">
-      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-8 md:gap-x-12 gap-y-20 md:gap-y-32">
+      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-8 md:gap-x-12 gap-y-20 md:gap-y-24">
         {articles.map((article) => (
           <Link href={`/article/${article.id}`} key={article.id} itemProp="url">
             <article>
